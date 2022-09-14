fe732e2c6decd35c3712cc4b9d9987cea4686b0c
aroma-o-teu-conforto <113542949+aroma-o-teu-conforto@users.noreply.github.com>
2022-09-14 18:16:57 +0100

    Add files via upload

---
+++ build/precache-manifest.01a86a55719527059b355c7d1e44b364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b84a084c035bccf3022",
+    "url": "/web-page/static/css/main.7c77ecbc.chunk.css"
+  },
+  {
+    "revision": "6b84a084c035bccf3022",
+    "url": "/web-page/static/js/main.6b84a084.chunk.js"
+  },
+  {
+    "revision": "9622e0642d479f7037f5",
+    "url": "/web-page/static/js/runtime~main.9622e064.js"
+  },
+  {
+    "revision": "d6799c1761d505f1dd1f",
+    "url": "/web-page/static/js/2.d6799c17.chunk.js"
+  },
+  {
+    "revision": "3de5ad028434bed6832c2d3d83926730",
+    "url": "/web-page/static/media/IMG_7384.3de5ad02.jpg"
+  },
+  {
+    "revision": "5baec0f5f59c1ea3caddde4961c87d2f",
+    "url": "/web-page/static/media/IMG_7392.5baec0f5.jpg"
+  },
+  {
+    "revision": "02fa167f073e98d927e6641a1294ea34",
+    "url": "/web-page/index.html"
+  }
+];
