4bcbd9ef91f385c6c36fe769780c8f3a1c861e3c
SirNanwar <fernando.guerra.duarte@gmail.com>
2022-09-14 18:52:41 +0100

    deploy

--- build/precache-manifest.db43de8616b2ff97959d7e98edfb5a3c.js
+++ build/precache-manifest.db43de8616b2ff97959d7e98edfb5a3c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "57be6c451e99a5c08b0f",
+    "revision": "af15d91fa3b6fb26e0a2",
     "url": "/web-page/static/css/main.8b2a6980.chunk.css"
   },
   {
-    "revision": "57be6c451e99a5c08b0f",
-    "url": "/web-page/static/js/main.57be6c45.chunk.js"
+    "revision": "af15d91fa3b6fb26e0a2",
+    "url": "/web-page/static/js/main.af15d91f.chunk.js"
   },
   {
     "revision": "9622e0642d479f7037f5",
     "url": "/web-page/static/js/runtime~main.9622e064.js"
   },
   {
-    "revision": "d6799c1761d505f1dd1f",
-    "url": "/web-page/static/js/2.d6799c17.chunk.js"
+    "revision": "b9141296c0127c4c6087",
+    "url": "/web-page/static/js/2.b9141296.chunk.js"
   },
   {
     "revision": "3de5ad028434bed6832c2d3d83926730",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/web-page/static/media/IMG_7392.5baec0f5.jpg"
   },
   {
-    "revision": "3fb58ffc7920f3175a74c070791ca717",
+    "revision": "48ce4c8fa8dd1d31fd1a89516c6d10ca",
     "url": "/web-page/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web-page/precache-manifest.3654ed7c11d805a001202fc7022c8f01.js"
+  "/web-page/precache-manifest.db43de8616b2ff97959d7e98edfb5a3c.js"
 );
 
 workbox.clientsClaim();
