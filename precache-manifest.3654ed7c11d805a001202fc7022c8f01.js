0ac53802a37600cf7c941da30c2d3703e8d69d36
SirNanwar <fernando.guerra.duarte@gmail.com>
2022-09-14 18:43:16 +0100

    Updates

---
+++ precache-manifest.3654ed7c11d805a001202fc7022c8f01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57be6c451e99a5c08b0f",
+    "url": "/web-page/static/css/main.8b2a6980.chunk.css"
+  },
+  {
+    "revision": "57be6c451e99a5c08b0f",
+    "url": "/web-page/static/js/main.57be6c45.chunk.js"
+  },
+  {
+    "revision": "9622e0642d479f7037f5",
+    "url": "/web-page/static/js/runtime~main.9622e064.js"
+  },
+  {
+    "revision": "d6799c1761d505f1dd1f",
+    "url": "/web-page/static/js/2.d6799c17.chunk.js"
+  },
+  {
+    "revision": "3de5ad028434bed6832c2d3d83926730",
+    "url": "/web-page/static/media/IMG_7384.3de5ad02.jpg"
+  },
+  {
+    "revision": "5baec0f5f59c1ea3caddde4961c87d2f",
+    "url": "/web-page/static/media/IMG_7392.5baec0f5.jpg"
+  },
+  {
+    "revision": "3fb58ffc7920f3175a74c070791ca717",
+    "url": "/web-page/index.html"
+  }
+];
